4a65f5b67d236c9c9bca92ac172f79a3d94ff4d3
Ldm01 <guillaume.impellizzeri@orange.fr>
2021-12-28 17:27:18 +0100

    ajout page d'administration (only visible par admin)

---
+++ admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>News en ligne - CSI</title>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/news.css">
+    <meta name="viewport" contect="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/f3b2d82c4d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include 'menu.php'; displayMenu('admin'); ?>
+</body>
+</html>
