d634882cc2cc897eb31fe21ee2af180906098016
Ldm01 <guillaume.impellizzeri@orange.fr>
2021-12-28 17:24:55 +0100

    connexion au compte associé

--- connectDb.php
+++ connectDb.php
@@ -1,14 +1,31 @@
 <?php
-
+session_start();
 try {
     // TODO: ADD SWITCH STATEMENT TO CONNECT ONLY THE USER ASSOCIATED
     // FOR THE MOMENT WE'RE CONNECTED AS ADMIN
-    $db = new PDO(
-        'pgsql:host=localhost;dbname=gestionnews',
-        'administrateur',
-        '',
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
+    if (!isset($_SESSION['id'])) {
+        $db = new PDO(
+            'pgsql:host=localhost;dbname=gestionnews',
+            'notconnecteduser',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    } elseif ($_SESSION['admin']) {
+        $db = new PDO(
+            'pgsql:host=localhost;dbname=gestionnews',
+            'adminuser',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    } else {
+        $db = new PDO(
+            'pgsql:host=localhost;dbname=gestionnews',
+            'abonneuser',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    }
+
 } catch (Exception $e) {
     die($e->getMessage());
 }
