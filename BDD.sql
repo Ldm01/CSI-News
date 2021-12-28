aa946f50ead10a962fc4893b9e0eb557e0bb3ebe
Ldm01 <guillaume.impellizzeri@orange.fr>
2021-12-28 17:24:17 +0100

    creation des tables 100% + droits et procedures register et login

--- BDD.sql
+++ BDD.sql
@@ -20,7 +20,7 @@ DROP TABLE IF EXISTS abonne;
 DROP TABLE IF EXISTS mot_cle;
 DROP TABLE IF EXISTS compte;
 
-
+CREATE TYPE etat AS ENUM ('validé','nonvalidé','echec');
 
 
 -- Table: public.Compte
@@ -29,10 +29,10 @@ DROP TABLE IF EXISTS compte;
 
 CREATE TABLE IF NOT EXISTS compte
 (
-    pseudo char NOT NULL,
-    mdp char NOT NULL,
+    pseudo varchar(30) NOT NULL,
+    mdp varchar(256)NOT NULL,
     PRIMARY KEY (pseudo)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -50,17 +50,17 @@ ALTER TABLE compte
 CREATE TABLE IF NOT EXISTS abonne
 (
     idAbonne serial NOT NULL,
-    pseudo char NOT NULL,
-    nom char NOT NULL,
-    prenom char NOT NULL,
-    email char NOT NULL,
+    pseudo varchar(30) NOT NULL,
+    nom varchar(50) NOT NULL,
+    prenom varchar(50) NOT NULL,
+    email varchar(70) NOT NULL,
     telephone integer,
     dateInscription date NOT NULL,
     admin boolean NOT NULL,
     confiance boolean NOT NULL,
     PRIMARY KEY (idAbonne),
     CONSTRAINT fk_compte FOREIGN KEY(pseudo) REFERENCES compte(pseudo)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -79,11 +79,11 @@ CREATE TABLE IF NOT EXISTS domaine
 (
     idDomaine serial NOT NULL,
     idAbonnePropo serial,
-    libelle char NOT NULL,
+    libelle varchar(30) NOT NULL,
     estAccepte boolean,
     PRIMARY KEY (idDomaine),
     FOREIGN KEY(idAbonnePropo) REFERENCES abonne(idAbonne)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -105,7 +105,7 @@ CREATE TABLE IF NOT EXISTS interet
     FOREIGN KEY(idAbonne) REFERENCES abonne(idAbonne),
     FOREIGN KEY(idDomaine) REFERENCES domaine(idDomaine),
     PRIMARY KEY (idAbonne)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -123,9 +123,9 @@ ALTER TABLE interet
 CREATE TABLE IF NOT EXISTS mot_cle
 (
     idMotCle serial NOT NULL,
-    libelle char NOT NULL,
+    libelle varchar(30) NOT NULL,
     PRIMARY KEY (idMotCle)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -148,17 +148,17 @@ CREATE TABLE IF NOT EXISTS news
     idMotCle1 serial,
     idMotCle2 serial,
     idMotCle3 serial,
-    titre char NOT NULL,
+    titre varchar(30) NOT NULL,
     contenu text COLLATE pg_catalog."default" NOT NULL,
     datePublication date NOT NULL,
-    etatN character(1) COLLATE pg_catalog."default" NOT NULL,
+    etatN etat NOT NULL,
     FOREIGN KEY(idAbonne) REFERENCES abonne(idAbonne),
     FOREIGN KEY(idDomaine) REFERENCES domaine(idDomaine),
     FOREIGN KEY(idMotCle1) REFERENCES mot_cle(idMotCle),
     FOREIGN KEY(idMotCle2) REFERENCES mot_cle(idMotCle),
     FOREIGN KEY(idMotCle3) REFERENCES mot_cle(idMotCle),
     PRIMARY KEY (idNews)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -181,11 +181,11 @@ CREATE TABLE IF NOT EXISTS archive_news
     idMotCle1 serial,
     idMotCle2 serial,
     idMotCle3 serial,
-    titre "char" NOT NULL,
+    titre varchar(30) NOT NULL,
     contenu text COLLATE pg_catalog."default" NOT NULL,
     datePublication date NOT NULL,
     dateArchivage date NOT NULL,
-    etatA char NOT NULL,
+    etatA etat NOT NULL,
     FOREIGN KEY(idAbonneP) REFERENCES abonne(idAbonne),
     FOREIGN KEY(idAbonneE) REFERENCES abonne(idAbonne),
     FOREIGN KEY(idDomaine) REFERENCES domaine(idDomaine),
@@ -193,7 +193,7 @@ CREATE TABLE IF NOT EXISTS archive_news
     FOREIGN KEY(idMotCle2) REFERENCES mot_cle(idMotCle),
     FOREIGN KEY(idMotCle3) REFERENCES mot_cle(idMotCle),
     PRIMARY KEY (idArchive)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -214,10 +214,10 @@ CREATE TABLE IF NOT EXISTS etude
     idNews serial,
     FOREIGN KEY(idAbonne) REFERENCES abonne(idAbonne),
     FOREIGN KEY(idNews) REFERENCES news(idNews),
-    justification "char",
+    justification varchar(255),
     dateEtude date,
     PRIMARY KEY (idAbonne)
-    )
+)
 
     TABLESPACE pg_default;
 
@@ -247,17 +247,73 @@ CREATE TABLE IF NOT EXISTS parametre
 ALTER TABLE parametre
     OWNER to postgres;
 
+--- Rôles et droits des différents comptes ---
 CREATE ROLE administrateur;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administrateur;
-ALTER ROLE administrateur WITH LOGIN;
-
+CREATE USER adminUser;
+GRANT administrateur TO adminUser;
 
 CREATE ROLE abonne;
-GRANT SELECT ON domaine, news, mot_cle, archive_news TO abonne;
-GRANT INSERT ON domaine, news, mot_cle TO abonne;
-ALTER ROLE abonne WITH LOGIN;
+GRANT SELECT ON compte, abonne, domaine, news, mot_cle, archive_news TO abonne;
+GRANT INSERT ON compte, abonne, domaine, news, mot_cle TO abonne;
+CREATE USER abonneUser;
+GRANT abonne TO abonneUser;
 
 CREATE ROLE utilisateur;
-GRANT SELECT ON news, mot_cle TO utilisateur;
-GRANT INSERT ON compte TO utilisateur;
-ALTER ROLE utilisateur WITH LOGIN;
+GRANT SELECT ON compte, abonne, news, mot_cle TO utilisateur;
+GRANT INSERT ON compte, abonne TO utilisateur;
+CREATE USER notConnectedUser;
+GRANT utilisateur TO notConnectedUser;
+
+--- Procédures ---
+CREATE OR REPLACE PROCEDURE Inscription(Ppseudo varchar(30),
+                                        Pmdp varchar(30),
+                                        Pnom varchar(50),
+                                        Pprenom varchar(50),
+                                        Pemail varchar(70),
+                                        Ptelephone integer)
+    LANGUAGE plpgsql
+AS $$
+DECLARE
+    PdateInscription date = current_date;
+BEGIN
+    IF (SELECT COUNT(*) FROM compte WHERE compte.pseudo = Ppseudo) <> 0 THEN
+        RAISE EXCEPTION 'Pseudo déjà existant';
+    END IF;
+    IF (SELECT COUNT(*) FROM abonne WHERE abonne.email = Pemail) <> 0 THEN
+        RAISE EXCEPTION 'Email déjà existant';
+    END IF;
+    IF Ppseudo = '' THEN
+        RAISE EXCEPTION 'Le pseudo est obligatoire';
+    END IF;
+    IF Pmdp = '' THEN
+        RAISE EXCEPTION 'Le mot de passe est obligatoire';
+    END IF;
+    IF Pnom = '' THEN
+        RAISE EXCEPTION 'Le nom est obligatoire';
+    END IF;
+    IF Pprenom = '' THEN
+        RAISE EXCEPTION 'Le prenom est obligatoire';
+    END IF;
+    IF Pemail = '' THEN
+        RAISE EXCEPTION 'Le mail est obligatoire';
+    END IF;
+
+
+    INSERT INTO compte VALUES(Ppseudo, Pmdp);
+    INSERT INTO abonne (nom, pseudo, prenom, email, telephone, dateInscription, admin, confiance)
+    VALUES(Pnom, Ppseudo, Pprenom, Pemail, Ptelephone, PdateInscription, false, false);
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION connexion (pseudonyme char, passwd char)
+    RETURNS boolean as $$
+DECLARE ok boolean;
+BEGIN
+    SELECT (mdp = $2) into ok
+    FROM compte
+    Where pseudo = $1;
+
+    RETURN ok;
+END;
+$$ LANGUAGE plpgsql;
