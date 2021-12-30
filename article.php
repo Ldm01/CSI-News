a1013b7c6368e4018701c14425569dd8de0d5776
Ldm01 <guillaume.impellizzeri@orange.fr>
2021-12-30 17:50:15 +0100

    affichage article avec l'id en paramètre d'url

--- article.php
+++ article.php
@@ -10,18 +10,9 @@
         <script src="https://kit.fontawesome.com/f3b2d82c4d.js" crossorigin="anonymous"></script>
     </head>
     <body>
-<?php include 'menu.php'; displayMenu('news'); ?>
+<?php include 'menu.php'; displayMenu('news'); include 'displayArticle.php';
 
-        <h2>TITRE DE L'ARTICLE</h2>
-        <p class="headerNews">Ecrit par Person le 00/00/00 à 00:00 | Catégorie : Sciences | Etat : En attente de validation</p>
-        <p class="contentNews"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nisi est, euismod commodo lacus vitae, lobortis commodo turpis. Integer mollis ante id velit condimentum, et lobortis nisl ultrices. Phasellus in dui nisl. Duis eget metus erat. Phasellus eget nisi erat. Vivamus dapibus malesuada diam nec aliquet. Duis nec elit quis odio pulvinar convallis. Aliquam vitae augue congue, egestas turpis in, fermentum quam. Quisque molestie tincidunt augue non volutpat. Etiam placerat tincidunt lorem, non ultricies nunc. Integer lacinia massa dui, eu commodo ex interdum quis. Nunc sollicitudin aliquet libero. Mauris aliquet, lacus ac fermentum pellentesque, nisi eros luctus sem, ut fermentum turpis felis ut quam. Maecenas nibh velit, efficitur ut nunc sit amet, euismod semper dolor.
-
-            Vestibulum fermentum urna eget neque aliquam, eget tincidunt enim luctus. Cras semper nunc in neque rutrum, sed congue risus molestie. In at est aliquam, efficitur velit a, ultrices elit. Integer consectetur magna vel est eleifend, cursus gravida sapien sollicitudin. Nulla facilisi. Fusce in hendrerit dolor. Aliquam erat volutpat. Sed feugiat, ipsum sit amet venenatis efficitur, orci magna dignissim nisi, a cursus eros velit ut quam. Morbi id turpis sit amet arcu tincidunt dignissim id a ex. Fusce eget ligula in velit iaculis pharetra. Aliquam fermentum vehicula odio, nec cursus neque ornare non. Duis ultrices est tempus, vulputate neque sed, egestas magna.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nisi est, euismod commodo lacus vitae, lobortis commodo turpis. Integer mollis ante id velit condimentum, et lobortis nisl ultrices. Phasellus in dui nisl. Duis eget metus erat. Phasellus eget nisi erat. Vivamus dapibus malesuada diam nec aliquet. Duis nec elit quis odio pulvinar convallis. Aliquam vitae augue congue, egestas turpis in, fermentum quam. Quisque molestie tincidunt augue non volutpat. Etiam placerat tincidunt lorem, non ultricies nunc. Integer lacinia massa dui, eu commodo ex interdum quis. Nunc sollicitudin aliquet libero. Mauris aliquet, lacus ac fermentum pellentesque, nisi eros luctus sem, ut fermentum turpis felis ut quam. Maecenas nibh velit, efficitur ut nunc sit amet, euismod semper dolor.
-
-            Vestibulum fermentum urna eget neque aliquam, eget tincidunt enim luctus. Cras semper nunc in neque rutrum, sed congue risus molestie. In at est aliquam, efficitur velit a, ultrices elit. Integer consectetur magna vel est eleifend, cursus gravida sapien sollicitudin. Nulla facilisi. Fusce in hendrerit dolor. Aliquam erat volutpat. Sed feugiat, ipsum sit amet venenatis efficitur, orci magna dignissim nisi, a cursus eros velit ut quam. Morbi id turpis sit amet arcu tincidunt dignissim id a ex. Fusce eget ligula in velit iaculis pharetra. Aliquam fermentum vehicula odio, nec cursus neque ornare non. Duis ultrices est tempus, vulputate neque sed, egestas magna.
-        </p>
-        SEULEMENT VU PAR L'ABONNE DE CONFIANCE QUI A ETE CHOISI ALEATOIREMENT POUR VALIDER
+        if ($state !== 'validé') {?>
         <fieldset style="text-align: center;" id="validateNews">
             <legend>Valider la news</legend>
             <form>
@@ -34,3 +25,6 @@
                 <input type="submit" value="Soumettre mon choix">
             </form>
         </fieldset>
+    <?php } ?>
+    </body>
+</html>
