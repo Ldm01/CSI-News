6f5e9e79b557c4e076c2a80b317682898197407f
Ldm01 <guillaume.impellizzeri@orange.fr>
2021-12-30 17:47:48 +0100

    update

--- connectDb.php
+++ connectDb.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 try {
-    // TODO: ADD SWITCH STATEMENT TO CONNECT ONLY THE USER ASSOCIATED
-    // FOR THE MOMENT WE'RE CONNECTED AS ADMIN
     if (!isset($_SESSION['id'])) {
         $db = new PDO(
             'pgsql:host=localhost;dbname=gestionnews',
@@ -25,7 +23,6 @@ try {
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
         );
     }
-
 } catch (Exception $e) {
     die($e->getMessage());
 }
